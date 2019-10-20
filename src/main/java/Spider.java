2b908d137c8696e97adde9e9f994245fe41861a5
wangzikui <934346501@qq.com>
2019-10-20 11:05:42 +0800

    first commit

---
+++ src/main/java/Spider.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class Spider {
+    private static ChromeDriverService service;
+    private static WebDriver webDriver;
+
+    /**
+     * 创建一个浏览器实例
+     * @return　webDriver
+     */
+
+    public WebDriver getChromeDriver() throws MalformedURLException {
+        System.setProperty("webdriver.chrome.driver","src/webDriver/chromedriver");
+        //创建一个　ChromeDriver 接口
+        service = new ChromeDriverService.Builder().usingDriverExecutable(new File(System.getProperty("webdriver.chrome.driver"))).usingAnyFreePort().build();
+        try {
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("ChromeDriverService启动异常");
+        }
+        //创建一个　chrome 浏览器实例
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("lang=zh_CN.UTF-8");
+        chromeOptions.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36");
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        dc.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        return new RemoteWebDriver(service.getUrl(), dc);
+    }
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        Spider spider = new Spider();
+        WebDriver webDriver = spider.getChromeDriver();
+        int i = 10;
+        while (i > 0) {
+            webDriver.get("https://www.baidu.com/");
+            System.out.println(webDriver.getTitle());
+            Thread.sleep(3000);
+            --i;
+        }
+
+        webDriver.close();
+    }
+}
